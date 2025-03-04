82fd71ab2142df65e5f70164d100b70dccadb8bc
safwantk-commerce9 <safwantkcommerce9@gmail.com>
2025-03-04 13:03:32 +0530

    get task by id

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -18,10 +18,22 @@ router.post("/", authMiddleware, async (req, res) => {
 // Get Tasks
 router.get("/", authMiddleware, async (req, res) => {
     try {
-        const tasks = await Task.find({ userId: req.user.userId });
+        const tasks = await Task.find({ userId: req.user.userId }).sort({ createdAt: -1 });
         res.json({ tasks, message: tasks.length ? "All Tasks fetched successfully" : "No Task Found" });
     } catch (error) {
         res.status(500).json({ error: "Error fetching tasks" });
+    }
+});
+
+//Get Task by Id
+
+router.get("/:id", authMiddleware, async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id, req.body, { new: true });
+        if (!task) return res.status(404).json({ error: "Task not found" });
+        res.json({ message: `Task fetched successfully`, task });
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching task" });
     }
 });
 
